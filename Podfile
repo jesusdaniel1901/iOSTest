2d499c029e1eb0ab8bfb0004433ee2f166d53f3c
Jesus Garcia <jesusdaniel1901@gmail.com>
2017-07-31 18:03:57 -0400

    clean up of pod file

--- Podfile
+++ Podfile
@@ -4,13 +4,7 @@ inhibit_all_warnings!
 use_frameworks!
 
 target 'iOSTest' do
-#  pod 'Alamofire', :git => 'https://github.com/Alamofire/Alamofire.git', :tag => '3.5.0'
     pod 'Alamofire', '~> 4.4'
-   # pod 'Gloss', '~> 1.2'
-    #pod 'SDWebImage', '~>3.8'
-   #pod 'CellRegistrable', :git => 'https://github.com/KogiMobileSAS/CellRegistrable', :branch => 'swift3'
-    pod 'Unbox'
-    pod 'SwiftyJSON'
-    #pod 'RxSwift'
+    pod 'SwiftyJSON'    
 end
 
--- iOSTest/Counter.swift
+++ iOSTest/Counter.swift
@@ -21,22 +21,4 @@ struct Counter {
     self.count = count
   }
   
-  
-  static func loadCounters() -> [Counter]{
-    var counters = [Counter]()
-    
-    let counterOne = Counter(id:"1234", title: "Jesus",count: 1)
-    let counterTwo = Counter(id:"1234",title: "Ricardo",count: 1)
-    let counterThree = Counter(id:"1234",title: "Osvaldo",count: 1)
-    
-    
-    counters.append(counterOne)
-    counters.append(counterTwo)
-    counters.append(counterThree)
-    
-    print(counters.count)
-    
-    return counters
-  }
-  
 }
